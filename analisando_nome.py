7f3bb1199daacf89c8dc435623a25756a024e85f
Fabio Souza <fabio.souza011@hotmail.com>
2022-09-09 10:55:45 -0300

    added program Analyze Name and Python_Tips.txt

---
+++ analisando_nome.py
@@ -0,0 +1,11 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+
+print('Analisando seu nome...')
+
+print(f'Seu nome completo em letra maiuscula é {nome.upper()}')
+
+print(f'Seu nome completo em letra minuscula é {nome.lower()}')
+
+print(f'Seu nome tem ao todo {len(nome) - nome.count(" ")} letras')
+
+print(f'Seu primeiro nome tem {nome.find(" ")} letras')
